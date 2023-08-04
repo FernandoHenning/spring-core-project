eaf6a7bd998e2a088c00b287f597d599dab8cd9b
FernandoHenning <fernando_hema@hotmail.com>
2023-08-04 13:10:49 -0600

    Created: CreateUserRequest to User mapping function

--- src/main/java/dev/fernandohenning/springcoreproject/mapper/UserMapper.java
+++ src/main/java/dev/fernandohenning/springcoreproject/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package dev.fernandohenning.springcoreproject.mapper;
 
+import dev.fernandohenning.springcoreproject.dto.user.request.CreateUserRequest;
 import dev.fernandohenning.springcoreproject.dto.user.response.GetUserInformationResponse;
 import dev.fernandohenning.springcoreproject.entity.User;
 import org.mapstruct.Mapper;
@@ -8,4 +9,6 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
     GetUserInformationResponse toGetUserInformation(User user);
+
+    User toUser(CreateUserRequest request);
 }
